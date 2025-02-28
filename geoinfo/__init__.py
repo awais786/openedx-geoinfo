7ae94970fb46d0c820219b1b11b8d5bef9c698a5
awais qureshi <awais.qureshi@arbisoft.com>
2025-02-28 17:48:02 +0500

    feat!: extracting survery app.

---
+++ geoinfo/__init__.py
@@ -0,0 +1,4 @@
+"""
+geoinfo module.
+"""
+__version__ = '0.1.0'
---
+++ settings/test.py
@@ -0,0 +1,14 @@
+"""Settings for use during testing."""
+
+# IN-MEMORY TEST DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+# END IN-MEMORY TEST DATABASE
