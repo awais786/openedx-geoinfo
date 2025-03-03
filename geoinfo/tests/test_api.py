cd53f9a044cc6b73e67210156f5e0ad3bdd4b1cd
awais qureshi <awais.qureshi@arbisoft.com>
2025-03-03 16:20:51 +0500

    feat!: extracting survery app.

--- geoinfo/tests/test_api.py
+++ geoinfo/tests/test_api.py
@@ -10,10 +10,10 @@ from geoinfo.api import country_code_from_ip
 class GeoIPTest(TestCase):
 
     @data(
-        ("8.8.8.8", "US"),  # Google Public DNS (USA)
-        ("1.1.1.1", "AU"),  # Cloudflare (Australia)
+        ("8.8.8.8", "US"),
+        ("116.58.86.0", "PK")
     )
     @unpack
-    def test_geoip_lookup(self, ip_addr, expected_country):
+    def test_country_code_from_ip(self, ip_addr, expected_country):
         country_code = country_code_from_ip(ip_addr)
         self.assertEqual(country_code, expected_country)
